453f3d975320310949bd379a6e2680415e235bfb
Darrick Robarge <darrickrobarge@gmail.com>
2022-09-13 20:43:16 -0400

    Comments Router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsiteDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage'
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='contact' element={<ContactPage />} />
           <Route path='directory' element={<CampsitesDirectoryPage />}/>
+          <Route path='directory/:campsiteId' element= {<CampsiteDetailPage />} />
       </Routes>
       <Footer />
     </div>
